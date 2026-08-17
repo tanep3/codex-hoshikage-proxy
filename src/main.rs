97d63193d8957f1233ef507670d64706dfe2b529
tane <tanemara@yahoo.co.jp>
2026-08-17 18:56:35 +0900

    fix: show proxy startup status by default

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use codex_hoshikage_proxy::{
     model::ModelRegistry,
     runtime::CodexRuntime,
 };
-use tracing_subscriber::EnvFilter;
+use tracing_subscriber::{EnvFilter, filter::LevelFilter};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_default_env())
+        .with_env_filter(EnvFilter::from_default_env().add_directive(LevelFilter::INFO.into()))
         .init();
     let config_path = default_config_path();
     let config = ValidatedConfig::load(&config_path)?;
